83002c79da1342c26dd10aedf5a994df23c6b9f1
jcabrera-ucu <101676632+jcabrera-ucu@users.noreply.github.com>
2023-03-24 21:52:51 +0000

    Cosas

--- main.py
+++ main.py
@@ -3,10 +3,15 @@
 def prob_A(A, o):
     return A / o
 
+def prob_A_dado_B(prob_B, prob_A_y_B):
+    return prob_A_y_B / prob_B
+
 if __name__ == "__main__":
     A = 3
     O = 6
 
-    print(f'Menor que 3: {prob_A(A, O)}')
+    print(f'<= 3: {prob_A(A, O)}')
     print(f'Impar: {prob_A(3, 6)}')
-    print(f'Menor que tres | Impar: {prob_A(2, 3)}')
+    print(f'<= 3 | Impar: {prob_A(2, 3)}')
+
+    print(f'Impar | <= 3: {prob_A_dado_B(3, 2)}')
