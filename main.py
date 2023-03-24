8878ee3cfc23e8870d2fff6950fb15670ac2f0ab
jcabrera-ucu <101676632+jcabrera-ucu@users.noreply.github.com>
2023-03-24 21:35:20 +0000

    Cosas

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 
+def prob_A(A, o):
+    return A / o
+
+if __name__ == "__main__":
+    A = 3
+    O = 6
+
+    print(f'Y este será nuestro resultado: {prob_A(A, O)}')
+    print(f'Impares: {prob_A(3, 6)}')
