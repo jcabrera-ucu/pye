a0afaa06e2d38f19996a64ddb3ec41cd1f023e2c
jcabrera-ucu <101676632+jcabrera-ucu@users.noreply.github.com>
2023-03-24 21:42:00 +0000

    Cosas

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     A = 3
     O = 6
 
-    print(f'Y este será nuestro resultado: {prob_A(A, O)}')
-    print(f'Impares: {prob_A(3, 6)}')
+    print(f'Menor que 3: {prob_A(A, O)}')
+    print(f'Impar: {prob_A(3, 6)}')
+    print(f'Menor que tres | Impar: {prob_A(2, 3)}')
